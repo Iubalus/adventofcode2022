f7b663c7e0a7780a144999aa03f547f054d302fb
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2022-12-12 22:21:37 -0700

    Add day 1 Part 2 solution

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -15,9 +15,25 @@ class Day1 {
             .max();
     }
 
+    fun findTop3HighestCalorieCountElf(input: String): Int {
+        val sep = System.lineSeparator()
+        return input
+            .split(sep + sep)
+            .map { i ->
+                i
+                    .split(sep)
+                    .map { v -> v.toInt() }
+                    .reduce { l, r -> l + r }
+            }
+            .sortedDescending()
+            .subList(0, 3)
+            .reduce({ l, r -> l + r });
+    }
+
 }
 
 fun main(args: Array<String>) {
     val input = Day1::class.java.getResourceAsStream("/day1/input.txt")?.bufferedReader()?.readText()
-    print(Day1().findHighestCalorieCountElf(input ?: "-1"));
+    println(Day1().findHighestCalorieCountElf(input ?: "-1"));
+    println(Day1().findTop3HighestCalorieCountElf(input ?: "-1"));
 }
